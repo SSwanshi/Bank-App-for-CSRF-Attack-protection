7d5b19defea3a182cd7b031472984670c1f74236
SSwanshi <adityaspgm@gmail.com>
2026-03-22 03:26:22 +0530

    prod fix

--- bank-frontend/src/utils/api.ts
+++ bank-frontend/src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5000", // Local development
-  baseURL: "https://bank-app-for-csrf-attack-protection.onrender.com", // Production
+  baseURL: "https://csrf.swanshi.me", // Production
   withCredentials: true, // 🔥 REQUIRED for cookies
 });
 
